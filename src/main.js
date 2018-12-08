c7ff074e19229d737d2d9bd5a184838cbe35b6b1
wzb <w342252891@foxmail.com>
2018-12-09 00:57:12 +0800

    added HomeHeader

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Mint from 'mint-ui'
+import $ from 'jquery'
+import './assets/styles/reset.css'
+import './assets/styles/iconfont.css'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import { TabContainer, TabContainerItem } from 'mint-ui';
+
+Vue.config.productionTip = false
+Vue.use(Mint)
+Vue.use(MintUI)
+Vue.component(TabContainer.name, TabContainer);
+Vue.component(TabContainerItem.name, TabContainerItem);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/components/HomeHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-back">
+      <div class="iconfont back-arrow">
+      &#xe600;
+      </div>
+    </div>
+    <div class="header-title">失物寻踪</div>
+    <div class="header-search">
+      <div class="iconfont search-icon">
+        &#xe60c;
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeHeader"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display flex
+    height .97rem
+    border-bottom 1px solid rgb(233, 233, 233)
+    .header-back
+      width 30px
+      height .97rem
+      line-height .97rem
+      margin-left .30rem
+      float left
+      .back-arrow
+        font-size .54rem
+    .header-title
+      flex 1
+      height .97
+      line-height .97rem
+      font-family Arial
+      color rgb(10, 9, 9)
+      font-size.44rem
+      text-align center
+    .header-search
+      width 30px
+      height .97rem
+      line-height .97rem
+      margin-right .30rem
+      float right
+      .search-icon
+        font-size .54rem
+</style>
